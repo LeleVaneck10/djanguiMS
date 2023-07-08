62f7d7415e8881c55adc2160de5801cac2403571
LeleVaneck10 <leletokamvaneckauriol@gmail.com>
2023-07-08 13:34:58 +0100

    set up

---
+++ src/main/java/com/lele/njangui/NjanguiApplication.java
@@ -0,0 +1,13 @@
+package com.lele.njangui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjanguiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjanguiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lele/njangui/NjanguiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lele.njangui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjanguiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
